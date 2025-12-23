2f965847c77d86c6c31cce823add96ec6628cfa6
dadaguaiup <feihongh062@gmail.com>
2025-12-23 20:26:10 +0800

    1.1.3

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-12-23 20:19:04
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-12-23 20:20:32
+ * @LastEditTime: 2025-12-23 20:29:19
  * @FilePath: \git_demo\helloworld.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,6 +12,7 @@
  int main(int argc, char const *argv[])
  {
     printf("hello world!");
+    printf("hello world2!");
     return 0;
  }
  
